966d9b5c7a9d87a82e2b5837efdf8c378450cd0b
Alex8943 <65241585+Alex8943@users.noreply.github.com>
2024-02-12 10:03:39 +0100

    added navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import { useState } from 'react'
 
-import './App.css'
-
+import { Grid, GridItem, Show } from '@chakra-ui/react'
+import NavBar from './components/Navbar'
 function App() {
   
 
   return (
     <>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`, 
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+  <GridItem pl='2' bg='orange.300' gridArea={'nav'}>
+    <NavBar/>
+  </GridItem>
+  <Show above="lg">
+    <GridItem pl='2' bg='pink.300' gridArea={'aside'}>
+      Aside
+    </GridItem>
+  </Show>
+  <GridItem pl='2' bg='green.300' gridArea={'main'}>
+    Main
+  </GridItem>
+</Grid>
    
     </>
   )
