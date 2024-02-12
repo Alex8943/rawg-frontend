8fe3a3d3e5ccdc89006f7bc631bbe9ad242867b1
Alex8943 <65241585+Alex8943@users.noreply.github.com>
2024-02-12 10:37:14 +0100

    add dark mode switch, and logo

--- src/App.tsx
+++ src/App.tsx
@@ -12,18 +12,18 @@ function App() {
         lg: `"nav nav" "aside main"`,
       }}
     >
-  <GridItem pl='2' bg='orange.300' gridArea={'nav'}>
-    <NavBar/>
-  </GridItem>
-  <Show above="lg">
-    <GridItem pl='2' bg='pink.300' gridArea={'aside'}>
-      Aside
+    <GridItem pl='2' bg='orange.300' gridArea={'nav'}>
+      <NavBar />
     </GridItem>
-  </Show>
-  <GridItem pl='2' bg='green.300' gridArea={'main'}>
-    Main
-  </GridItem>
-</Grid>
+    <Show above="lg">
+      <GridItem pl='2' bg='pink.300' gridArea={'aside'}>
+        Aside
+      </GridItem>
+    </Show>
+    <GridItem pl='2' bg='green.300' gridArea={'main'}>
+      Main
+    </GridItem>
+  </Grid>
    
     </>
   )
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import { HStack, Image } from '@chakra-ui/react'
+import logo from '../assets/logo.webp'
+import ColorModeSwitch from './ColorModeSwitch';
+
 
 const NavBar = () => {
-    return <div>Navbar</div>
-}; 
+  return (
+    <HStack justifyContent='space-between' padding='10px'>
+      <Image src={logo} boxSize='60px' />
+      <ColorModeSwitch />
+    </HStack>
+  )
+}
 
 export default NavBar
